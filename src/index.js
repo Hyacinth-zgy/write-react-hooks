31e524a5aedac935f2ca6499793a4d25f175dce4
Hyacinth-zgy <838673371@qq.com>
2021-04-30 14:31:45 +0800

    realized  react userHook usestate memo

--- src/index.js
+++ src/index.js
@@ -1,34 +1,52 @@
-// REACT-HOOKS -useState() 初步实现
+// REACT-HOOKS -useState() 的具象实现
+// 思路:用一个数组memoizedState来保存useState所用到的变量
+// 每次Counter重新执行都要重新使用useState依次从memoizedState中读取index赋值，所以18行的cursor要置为0
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-let lastState; // 声明一个状态值:代表值的上一个状态
+let memoizedState = []; // hooks 的值存放在这个数组里
+let cursor = 0; // 当前 memoizedState 的索引
+let number = 0;
 function useState(initialState) {
-  lastState = lastState || initialState; // 如果上一个状态有值的话就不使用初始值
+  number = number + 1;
+  memoizedState[cursor] = memoizedState[cursor] || initialState;
+  const currentCursor = cursor;
   function setState(newState) {
-    // 声明一个函数，将新的状态值赋值给lastState
-    console.log(1);
-    lastState = newState;
-    render(); // 执行重新渲染
+    memoizedState[currentCursor] = newState;
+    cursor = 0;
+    render();
   }
-  return [lastState, setState];
+  console.log(number);
+  return [memoizedState[cursor++], setState];
 }
 
+const ReactObj = {
+  useState,
+};
+console.log(ReactObj);
+
 function Counter() {
-  console.log(2);
-  let [state, setState] = useState(1);
+  let [state, setState] = ReactObj.useState(1);
+  let [stateString, setStateString] = ReactObj.useState('zgy');
   return (
     <div>
       <p>{state}</p>
+      <p>{stateString}</p>
       <button
         onClick={() => {
-          console.log(1);
           setState(state + 1);
         }}
       >
         点我加一
+      </button>
+      <button
+        onClick={() => {
+          setStateString(stateString + 'll');
+        }}
+      >
+        点我改变string
       </button>
     </div>
   );
